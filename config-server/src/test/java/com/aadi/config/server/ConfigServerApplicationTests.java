ace718af9ebd00484cd3bbf8e8b7954792817e12
aditya <adityatiwari210502@gmail.com>
2025-01-09 22:09:46 +0530

    Initial commit with microservices folder

---
+++ config-server/src/test/java/com/aadi/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadi.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
